507ff58d18398de50d211c93bdad7fa7e4478e24
aaron <aaronshort19@gmail.com>
2024-01-08 15:01:17 -0600

    walletTest1

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // help
-const word = "HTML"
+// const word = "HTML"
 
-const changeWord = () => {
-    document.getElementById("wordToChange").innerHTML = "REACT"
-}
+// const changeWord = () => {
+//     document.getElementById("wordToChange").innerHTML = "REACT"
+// }
 
-changeWord()
+// changeWord()
+
+
+import {
+    ThirdwebProvider,
+    ConnectWallet,
+    embeddedWallet,
+  } from "@thirdweb-dev/react";
+
+  export default function App() {
+    return (
+      <ThirdwebProvider
+        activeChain="goerli"
+        clientId="YOUR_CLIENT_ID"
+        supportedWallets={[embeddedWallet()]}
+      >
+        <ConnectWallet />
+      </ThirdwebProvider>
+    );
+  }
